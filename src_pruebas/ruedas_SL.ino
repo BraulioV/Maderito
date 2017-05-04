dbc0e06687509ea1a4ab4c49a335455024ec3bd0
Paula <terceranexus6@users.noreply.github.com>
2017-05-04 12:59:04 -0400

    Create ruedas_SL.ino

---
+++ src_pruebas/ruedas_SL.ino
@@ -0,0 +1,52 @@
+
+//control de los motores
+
+const int IN1 = 4;
+const int IN2 = 5;
+const int IN3 = 6;
+const int IN4 = 7;
+
+const int ENA = 9;
+const int ENB = 3;
+
+//siguelinea
+
+const int SLA = 12;
+const int SLB = 13;
+int sensorValA, sensorValB;
+
+
+void setup() {
+
+  pinMode (IN1, OUTPUT);
+  pinMode (IN2, OUTPUT);
+  pinMode (IN3, OUTPUT);
+  pinMode (IN4, OUTPUT);
+  pinMode (ENA, OUTPUT);
+  pinMode (ENB, OUTPUT);
+  // put your setup code here, to run once:
+  //Serial.begin(9600); 
+
+}
+
+void loop() {
+
+  //sensorValA = digitalRead(SLA); //Guardamos la lectura del pin Analógico
+  //sensorValB = digitalRead(SLB);
+
+  analogWrite(ENA, 255);
+  analogWrite(ENB, 255); 
+
+  //high para delante y low para atrás
+
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, HIGH);
+ 
+  
+  // put your main code here, to run repeatedly:
+
+  //Serial.println(sensorValB); //Sacamos la lectura por serial
+
+}
