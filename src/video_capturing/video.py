92e9a68cd6a2b474c6af98552b851d6421170696
BraulioV <brauliovarlop@correo.ugr.es>
2017-05-04 16:52:44 +0200

    deteccion de puntos en dos camaras

--- src/video_capturing/video.py
+++ src/video_capturing/video.py
@@ -10,8 +10,9 @@
 import numpy as np
 import cv2
 
-cap0 = cv2.VideoCapture(0)
-cap1 = cv2.VideoCapture(1)
+cap0 = cv2.VideoCapture(1)
+cap1 = cv2.VideoCapture(2)
+ffdetector = cv2.FastFeatureDetector_create()
 
 while(True):
     # Capture frame-by-frame
@@ -21,13 +22,23 @@ while(True):
     # Our operations on the frame come here
     gray0 = cv2.cvtColor(frame0, cv2.COLOR_BGR2GRAY)
     gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    
+    keypoints0 = ffdetector.detect(image=gray0, mask=None)
+    keypoints1 = ffdetector.detect(image=gray1, mask=None)
+
+    result1 = np.zeros(gray0.shape, np.int8)
+    result2 = np.zeros(gray1.shape, np.int8)
+
+    result1 = cv2.drawKeypoints(gray0, keypoints0, result1)
+    result2 = cv2.drawKeypoints(gray1, keypoints1, result2)
 
     # Display the resulting frame
-    cv2.imshow('Camera 0',gray0)
-    cv2.imshow('Camera 1',gray1)
+    cv2.imshow('Camera 0',result1)
+    cv2.imshow('Camera 1',result2)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
-cap.release()
+# cap0.release()
+cap1.release()
 cv2.destroyAllWindows()
