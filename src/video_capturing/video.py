62a13e0b52f4d849222a6263d6515834fc507ad3
BraulioV <brauliovarlop@correo.ugr.es>
2017-05-11 17:11:38 +0200

    probando goodfeaturestotrack

--- src/video_capturing/video.py
+++ src/video_capturing/video.py
@@ -11,30 +11,37 @@ import numpy as np
 import cv2
 
 cap0 = cv2.VideoCapture(1)
-cap1 = cv2.VideoCapture(2)
-ffdetector = cv2.xfeatures2d.SURF_create()
+# cap1 = cv2.VideoCapture(2)
+# ffdetector = cv2.xfeatures2d.SURF_create()
 
 while(True):
     # Capture frame-by-frame
     ret0, frame0 = cap0.read()
-    ret1, frame1 = cap1.read()
+    ret1, frame1 = cap0.read()
 
     # Our operations on the frame come here
-    gray0 = cv2.cvtColor(frame0, cv2.COLOR_BGR2GRAY)
-    gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    gray_prev = cv2.cvtColor(frame0, cv2.COLOR_BGR2GRAY)
+    gray_next = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    corners = cv2.goodFeaturesToTrack(gray_prev, 500,  0.01, 10)
+
+    prev_PyVR, next_PyVR = np.zeros(gray_prev.shape, dtype=np.uint8), np.zeros(gray_prev.shape, dtype=np.uint8)
     
-    keypoints0 = ffdetector.detect(image=gray0, mask=None)
-    keypoints1 = ffdetector.detect(image=gray1, mask=None)
+    curr_points, status, err = cv2.CalcOpticalFlowPyrLK(gray_prev, gray_next, 
+                                                                                prev_PyVR, next_PyVR,  corners, (10,10), 3 , (cv.CV_TERMCRIT_ITER|cv.CV_TERMCRIT_EPS,20, 0.03), 0)
+    # keypoints0 = ffdetector.detect(image=gray_prev, mask=None)
+    # keypoints1 = ffdetector.detect(image=gray_next, mask=None)
 
-    result1 = np.zeros(gray0.shape, np.int8)
-    result2 = np.zeros(gray1.shape, np.int8)
+    # result1 = np.zeros(gray_prev.shape, np.int8)
+    # result2 = np.zeros(gray_next.shape, np.int8)
 
-    result1 = cv2.drawKeypoints(gray0, keypoints0, result1)
-    result2 = cv2.drawKeypoints(gray1, keypoints1, result2)
+    # result1 = cv2.drawKeypoints(gray_prev, corners, result1)
+    # result2 = cv2.drawKeypoints(gray_next, keypoints1, result2)
+    # for i in corners:
+        # cv2.circle(frame0, (i[0][0], i[0][1]), 10, (255, 0, 0), -1)
 
     # Display the resulting frame
-    cv2.imshow('Camera 0',result1)
-    cv2.imshow('Camera 1',result2)
+    cv2.imshow('Camera 0',frame0)
+    # cv2.imshow('Camera 1',result2)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
