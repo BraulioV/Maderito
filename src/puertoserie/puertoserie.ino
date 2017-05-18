1810ee5274056957858927f272d64645a6a01c4e
mgmacias95 <mgmacias95@correo.ugr.es>
2017-05-18 16:43:23 +0200

    alguien que me ayude?

---
+++ src/puertoserie/puertoserie.ino
@@ -0,0 +1,23 @@
+const int ledPin = 0;
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  if (Serial.available() > 0) {
+    if (Serial.read() == 's') {
+      digitalWrite(ledPin, HIGH);
+      delay(2000);
+    }
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+  
+}
+
+
