b0076bf4462aeb6ab194a4a8e604986bb2765279
Paula <terceranexus6@users.noreply.github.com>
2017-05-25 10:56:52 -0400

    Update sensor_linea_y_motores.ino

--- src_pruebas/ruedas_SL/sensor_linea_y_motores.ino
+++ src_pruebas/ruedas_SL/sensor_linea_y_motores.ino
@@ -90,4 +90,16 @@ void loop() {
       digitalWrite(IN4, LOW);
   }
   
+    //girar a la derecha
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+
+  //girar a la izquierda
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  
 }
