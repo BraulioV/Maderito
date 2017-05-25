a786ccfe566e08a339fcecdc7cca2e5b730cfe3e
Paula <terceranexus6@users.noreply.github.com>
2017-05-25 11:13:16 -0400

    codigo menos guarrillo
    y comentarios derecha/izquierda

--- src_pruebas/ruedas_SL/sensor_linea_y_motores.ino
+++ src_pruebas/ruedas_SL/sensor_linea_y_motores.ino
@@ -87,6 +87,22 @@ void loop() {
       analogWrite(ENA, 155);
       analogWrite(ENB, 155);  
   }
+  
+  /*
+  
+  //girar a la derecha
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+
+  //girar a la izquierda
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  
+  */
 
   
 }
