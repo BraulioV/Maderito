1b7d3ee8e97aacadabe11a1283c3bbfcb45e6ef3
mgmacias95 <mgmacias95@correo.ugr.es>
2017-06-01 18:29:53 +0200

    añado ultrasonidos

--- sensor_linea_y_motores/sensor_linea_y_motores.ino
+++ sensor_linea_y_motores/sensor_linea_y_motores.ino
@@ -1,3 +1,6 @@
+const int EchoPin = 8;
+const int TriggerPin = 2;
+
 //control de los motores
 
 const int IN1 = 4;
@@ -21,10 +24,10 @@ const int SLB = 13;
 int sensorValA, sensorValB, sensorValC, sensorValD;
 
 void avanzar(){
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
 }
 
 void retroceder(){
@@ -35,89 +38,95 @@ void retroceder(){
 }
 
 void setup() {
-
-  pinMode (IN1, OUTPUT);
-  pinMode (IN2, OUTPUT);
-  pinMode (IN3, OUTPUT);
-  pinMode (IN4, OUTPUT);
-  pinMode (ENA, OUTPUT);
-  pinMode (ENB, OUTPUT);
-  pinMode (SLA, INPUT);
-  pinMode (SLB, INPUT);
-  pinMode (SLA_2, INPUT);
-  pinMode (SLB_2, INPUT);
-  Serial.begin(9600); 
-
+    pinMode (IN1, OUTPUT);
+    pinMode (IN2, OUTPUT);
+    pinMode (IN3, OUTPUT);
+    pinMode (IN4, OUTPUT);
+    pinMode (ENA, OUTPUT);
+    pinMode (ENB, OUTPUT);
+    pinMode (SLA, INPUT);
+    pinMode (SLB, INPUT);
+    pinMode (SLA_2, INPUT);
+    pinMode (SLB_2, INPUT);
+    pinMode(TriggerPin, OUTPUT);
+    pinMode(EchoPin, INPUT);
+    Serial.begin(9600); 
 }
 
 void loop() {
-  analogWrite(ENA, 60);
-  analogWrite(ENB, 60); 
-
-  //high para delante y low para atrás
-  //variables de los siguelinea
-  sensorValA = digitalRead(SLA);
-  sensorValB = digitalRead(SLB);
-  sensorValC = digitalRead(SLB_2);
-  sensorValD = digitalRead(SLA_2);
-
-  //muestra por serial de los valores (0 o 1)
-  Serial.println("Sensor A: ");
-  Serial.println(sensorValA);
-  Serial.println("Sensor B: " + sensorValB);
-  Serial.println("Sensor C: " + sensorValC);
-  Serial.println("Sensor D: " + sensorValD);
-
-  
-  if( sensorValC == 0 || sensorValD == 0 ) //linea negra delante derehca
-  {   
+    /*analogWrite(ENA, 60);
+    analogWrite(ENB, 60); 
+    
+    //high para delante y low para atrás
+    //variables de los siguelinea
+    sensorValA = digitalRead(SLA);
+    sensorValB = digitalRead(SLB);
+    sensorValC = digitalRead(SLB_2);
+    sensorValD = digitalRead(SLA_2);
+    
+    //muestra por serial de los valores (0 o 1)
+    Serial.println("Sensor A: ");
+    Serial.println(sensorValA);
+    Serial.println("Sensor B: " + sensorValB);
+    Serial.println("Sensor C: " + sensorValC);
+    Serial.println("Sensor D: " + sensorValD);
+    
+    
+    if( sensorValC == 0 || sensorValD == 0 ) //linea negra delante derehca
+    {   
       avanza = false;
       //mover maderito hacia atrás
       retroceder();
       delay(100);
-  }
-  else if( sensorValA == 0 || sensorValB == 0 )
-  {
+    }
+    else if( sensorValA == 0 || sensorValB == 0 )
+    {
         avanza = true;
         //mover maderito hacia delante
         avanzar();
         delay(100);
-   } 
-   else {
+    } 
+    else {
       if (avanza){
         avanzar();
       }
       else{
         retroceder();
       }
-   }
+    }*/
+    int cm = ping(TriggerPin, EchoPin);
+    Serial.println(cm);
+    delay(200);
+    /*
+    
+    //girar a la derecha
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    
+    //girar a la izquierda
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    
+    */
+    
 
-//  if( digitalRead(pirPin) == HIGH )
-//  {
-//      analogWrite(ENA, 255);
-//      analogWrite(ENB, 255); 
-//  }
-//  else if( digitalRead(pirPin) == LOW )
-//  {
-//      analogWrite(ENA, 155);
-//      analogWrite(ENB, 155);  
-//  }
-  
-  /*
-  
-  //girar a la derecha
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
+}
 
-  //girar a la izquierda
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-  
-  */
-
-  
+int ping(int TriggerPin, int EchoPin) {
+   long duration, distanceCm;
+   
+   digitalWrite(TriggerPin, LOW);  //para generar un pulso limpio ponemos a LOW 4us
+   delayMicroseconds(4);
+   digitalWrite(TriggerPin, HIGH);  //generamos Trigger (disparo) de 10us
+   delayMicroseconds(10);
+   digitalWrite(TriggerPin, LOW);
+   
+   duration = pulseIn(EchoPin, HIGH);  //medimos el tiempo entre pulsos, en microsegundos
+   
+   distanceCm = duration * 10 / 292/ 2;   //convertimos a distancia, en cm
+   return distanceCm;
 }
