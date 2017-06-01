ff3cbb3b6dbbcdb4af89b1716e55280e6085783b
BraulioV <brauliovarlop@correo.ugr.es>
2017-06-01 15:50:07 +0200

    arreglando el fichero de los motores

---
+++ src_pruebas/ruedas_SL/sensor_linea_y_motores/sensor_linea_y_motores.ino
@@ -0,0 +1,108 @@
+//control de los motores
+
+const int IN1 = 4;
+const int IN2 = 5;
+const int IN3 = 6;
+const int IN4 = 7;
+
+const int ENA = 9;
+const int ENB = 3;
+
+//distancia
+int pirPin = A2;
+
+//siguelinea
+
+
+const int SLA_2 = 10;
+const int SLB_2 = 11;
+const int SLA = 12;
+const int SLB = 13;
+int sensorValA, sensorValB, sensorValC, sensorValD;
+
+
+void setup() {
+
+  pinMode (IN1, OUTPUT);
+  pinMode (IN2, OUTPUT);
+  pinMode (IN3, OUTPUT);
+  pinMode (IN4, OUTPUT);
+  pinMode (ENA, OUTPUT);
+  pinMode (ENB, OUTPUT);
+  pinMode (SLA, INPUT);
+  pinMode (SLB, INPUT);
+  Serial.begin(9600); 
+
+}
+
+void loop() {
+  analogWrite(ENA, 255);
+  analogWrite(ENB, 255); 
+
+  //high para delante y low para atrás
+/*
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  */
+
+  //variables de los siguelinea
+  sensorValA = digitalRead(SLA);
+  sensorValB = digitalRead(SLB);
+  sensorValC = digitalRead(SLB_2);
+  sensorValD = digitalRead(SLA_2);
+
+  //muestra por serial de los valores (0 o 1)
+  Serial.println(sensorValA);
+  Serial.println(sensorValB);
+  Serial.println(sensorValC);
+  Serial.println(sensorValD);
+
+  
+  if( sensorValA == 0 || sensorValB == 0) //linea negra delante derehca
+  {
+      //mover maderito hacia atrás
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, HIGH);
+  }
+  else if( sensorValC == 0 || sensorValD == 0)
+  {
+        //mover maderito hacia delante
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, HIGH);
+        digitalWrite(IN3, HIGH);
+        digitalWrite(IN4, LOW);
+   }
+
+  if( digitalRead(pirPin) == HIGH )
+  {
+      analogWrite(ENA, 255);
+      analogWrite(ENB, 255); 
+  }
+  else if( digitalRead(pirPin) == LOW )
+  {
+      analogWrite(ENA, 155);
+      analogWrite(ENB, 155);  
+  }
+  
+  /*
+  
+  //girar a la derecha
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+
+  //girar a la izquierda
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  
+  */
+
+  
+}
